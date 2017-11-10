4ed5be37cf6030ba01f1aa3cd2263ec82020ef93
Sam <swkane@indiana.edu>
2017-11-10 15:10:14 -0500

    p4 functionality

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // P1 / P3
-let input = document.getElementsByName('given')[0]
+let input1 = document.getElementById('input1');
 let handleAnagram = () => {
-  console.log("Input Word: ",input.value);
-  let givenWord = input.value;
+  console.log("Input Word: ",input1.value);
+  let givenWord = input1.value;
   let alphabetizedWord = alphabetize(givenWord);
   let anagrams = words.filter((word) => {
     return alphabetize(word) === alphabetizedWord;
@@ -16,9 +16,8 @@ function alphabetize(word) {
 }
 
 // P2
+let dictionary = {};
 function makeDictionary() {
-  let dictionary = {};
-
   for (var i = 0; i < words.length; i++) {
     let alphabatized = alphabetize(words[i]);
     if (alphabatized in dictionary) {
@@ -28,16 +27,49 @@ function makeDictionary() {
     }
   }
   let fiveDictionary = {};
-  for (alphabatized in dictionary) {
-    if (dictionary[alphabatized].length >= 5) {
-      fiveDictionary[alphabatized] = dictionary[alphabatized];
+  for (prop in dictionary) {
+    if (dictionary[prop].length >= 5) {
+      fiveDictionary[prop] = dictionary[prop];
     }
   }
   return fiveDictionary;
 }
-
-
 // P4
 
 // Input: a word or phrase
 // Output: all two word anagrams to use all the letters
+
+let input2 = document.getElementById('input2');
+let handleAnagramPhrase = () => {
+  makeDictionary();
+  let keys = Object.keys(dictionary);
+  let givenPhrase = alphabetize(input2.value.split(" ").join("").toLowerCase());
+  console.log(givenPhrase);
+  let filteredKeys = [];
+  for (let i=0; i < keys.length; i++) {
+    // check to see if the word has letters that are not in the given
+    let wrongLetters = 0;
+    for (let j=0; j < keys[i].length; j++) {
+      if (givenPhrase.indexOf(keys[i][j]) === -1) {
+        wrongLetters ++;
+      }
+    }
+    if (wrongLetters === 0) {
+      filteredKeys.push(keys[i]);
+    }
+}
+  console.log("FilteredKeys: ", filteredKeys);
+  let twoWordAnagrams = [];
+  for (let i = 0; i < filteredKeys.length; i++) {
+    for (let j = i + 1; j < filteredKeys.length; j++) {
+      let anagramCombo = filteredKeys[i] + filteredKeys[j];
+      if (anagramCombo.length == givenPhrase.length) {
+        if (alphabetize(anagramCombo) === givenPhrase) {
+          twoWordAnagrams.push(`${dictionary[filteredKeys[i]]} + ${dictionary[filteredKeys[j]]}`);
+        }
+      }
+    }
+  }
+  console.log("twoWordAnagrams: ", twoWordAnagrams);
+  return twoWordAnagrams;
+}
